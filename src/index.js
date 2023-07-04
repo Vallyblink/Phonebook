17d1a8094e204e5d0b8e77e2c19d354a110918bc
Vallyblink <alabama.toj@gmail.com>
2023-07-04 14:32:32 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-       <BrowserRouter>
+       <BrowserRouter basename='https://Vallyblink.github.io/goit-react-hw-08-phonebook/'>
           <App />
         </BrowserRouter>
       </PersistGate>
